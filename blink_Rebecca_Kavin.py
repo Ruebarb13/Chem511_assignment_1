d014f49c972d4aab1065247b16fdcecb54698590
Ruebarb13 <112659029+Ruebarb13@users.noreply.github.com>
2026-01-12 08:39:10 -0800

    Add files via upload

--- blink_Rebecca_Kavin.py
+++ blink_Rebecca_Kavin.py
@@ -6,12 +6,40 @@
 
 from time import sleep_ms
 from machine import Pin
+import utime
 i = 0
 
-led = Pin(2, Pin.OUT)
-while i < 10:
-    led.value(1)
-    sleep_ms(200)
-    led.value(0)
-    sleep_ms(200)
-     i += 1
+#led = Pin(2, Pin.OUT)
+#while i<10:
+#    led.value(1)
+#    sleep_ms(200)
+#    led.value(0)
+#    sleep_ms(200)
+#    i+=1
+#    print(f"I'm smarter than Kavin {i}")
+    
+led = Pin(12, Pin.OUT)
+button = Pin(14, Pin.IN, Pin.PULL_UP)
+i=0
+while i==0:
+    if button.value()==0:
+        led.value(1)
+        #i+=1
+        print(f"doo doo doo")
+    else:
+        led.value(0)
+        utime.sleep(0.1)
+        #i+=1
+        print(f"I'm way smarter than Kavin {i}")
+        
+        
+#while i<20:    
+#    led.value(1)
+#    sleep_ms(200)
+#    led.value(0)
+#    sleep_ms(200)
+#    i+=1
+#    print(f"I'm way smarter than Kavin {i}")
+    
+
+    
